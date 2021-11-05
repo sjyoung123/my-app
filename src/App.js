5993dad955a35f307125baabd7524302c5f67d26
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2021-11-05 19:40:12 +0900

    Updated useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { useState, useEffect } from "react";
 import Button from "./Button";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const onClick = () => setCount((prev) => prev + 1);
+  useEffect(() => {
+    console.log("render only once");
+  }, []);
+
   return (
     <div>
-      <h1>hi!!</h1>
-      <Button text="Click Me" />
+      <h1>{count}</h1>
+      <Button onClick={onClick} text="Click Me" />
     </div>
   );
 }
--- src/Button.js
+++ src/Button.js
@@ -1,12 +1,17 @@
 import PropTypes from "prop-types";
 import styles from "./Button.module.css";
 
-function Button({ text }) {
-  return <button className={styles.btn}>{text}</button>;
+function Button({ text, onClick }) {
+  return (
+    <button onClick={onClick} className={styles.btn}>
+      {text}
+    </button>
+  );
 }
 
 Button.prototype = {
   text: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
 };
 
 export default Button;
