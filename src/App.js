c0f2f4c8cfd0ef30172b6488fd47474d0614e9e6
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2021-11-07 14:48:13 +0900

    Updated to-do-list

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
+import { string } from "prop-types";
 import { useState, useEffect } from "react";
 
-function Hello() {
-  // const byeFunc = () => {
-  //   console.log("Bye :(");
-  // };
-  // const hiFunc = () => {
-  //   console.log("Hi :)");
-  //   return byeFunc; //cleanup func
-  // };
-  // useEffect(hiFunc, []);
-
-  useEffect(() => {
-    console.log("hi:)");
-    return () => console.log("bye:("); //cleanup func
-  }, []);
-
-  return <h1>Hello</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (!toDo) {
+      return;
+    }
+    setToDos((prevArray) => [...prevArray, toDo]); //[toDo].concat(prevArray)
+    setToDo("");
+  };
+  const deleteList = (event) => {
+    const li = event.target.parentElement;
+    setToDos(() => toDos.filter((item) => item !== li.className));
+  };
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "hide" : "show"}</button>
+      <h1>My To-Do List({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} type="text"></input>
+        <button>Click</button>
+      </form>
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index} id={index} className={item}>
+            {item}
+            <span onClick={deleteList}>❌</span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
