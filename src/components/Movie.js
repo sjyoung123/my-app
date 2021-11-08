751f7dc12df761a00cd23f06b1c9b371c449bb17
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2021-11-08 17:29:57 +0900

    Updated movie-app
    updated detail page

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
-function Movie({ coverImg, title, summary, genres }) {
+function Movie({ id, coverImg, title, summary, genres }) {
   return (
     <div>
-      <img src={coverImg} />
+      <img src={coverImg} alt={title} />
       <h2>
-        <Link to="/movie">{title}</Link>
+        <Link to={`/movie/${id}`}>{title}</Link>
       </h2>
 
       <p>{summary}</p>
@@ -17,4 +18,12 @@ function Movie({ coverImg, title, summary, genres }) {
     </div>
   );
 }
+
+Movie.propTypes = {
+  id: PropTypes.number.isRequired,
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 export default Movie;
--- src/routers/Detail.js
+++ src/routers/Detail.js
@@ -1,4 +1,47 @@
+import { useCallback, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 function Detail() {
-  return <h1>Detail</h1>;
+  const { id } = useParams();
+  const [movieDetail, setMovieDetail] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const getMovieDetail = useCallback(async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setMovieDetail(json.data.movie);
+    setLoading(false);
+  }, [id]);
+
+  useEffect(() => {
+    getMovieDetail();
+  }, [getMovieDetail]);
+
+  console.log(movieDetail);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <h1>{movieDetail.title_long}</h1>
+          <img
+            src={movieDetail.large_cover_image}
+            alt={movieDetail.title_long}
+          />
+          <div>
+            <span>{movieDetail.description_full}</span>
+          </div>
+          <div>
+            <span>Rating: {movieDetail.rating}</span>
+          </div>
+          <div>
+            <span>Run-time: {movieDetail.runtime}min.</span>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 export default Detail;
